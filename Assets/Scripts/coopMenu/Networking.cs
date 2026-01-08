a18d9e083045fde46c20d36f0a655f7b23a371de
Wekúr <79259539+Wekur@users.noreply.github.com>
2026-01-08 13:15:19 +0100

    structure fix

--- Assets/Scripts/coopMenu/Networking.cs
+++ Assets/Scripts/coopMenu/Networking.cs
@@ -1,14 +1,27 @@
+/*
+    this script is responsible for handling network traffic for coop games, 
+    such as LAN connections or also global games via server connection 
+
+    @author: wek56ur@gmail.com
+    @date: 2026-01-08
+
+*/
+
 using UnityEngine;
 
-public class Networking : MonoBehaviour
+
+public class Networking : MonoBehaviour //not sure if gotta be MB
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    //should initiate connection and setup network objects 
     void Start()
     {
         
     }
 
     // Update is called once per frame
+    //runs TCP/IP stacks and updates data
     void Update()
     {
         
